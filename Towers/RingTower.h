d2f8ff2abf9258a40f2e412dfc4049541f2ab201
Hithesh Yedlapati <yedlapat@msu.edu>
2020-10-14 17:46:00 -0400

    Create constructors for each derived class

--- Towers/RingTower.h
+++ Towers/RingTower.h
@@ -3,5 +3,24 @@
 class CRingTower :
 	public CTower
 {
+public:
+	/// constructor
+	CRingTower(CGame* game);
+
+	/// Default constructor (disabled)
+	CRingTower() = delete;
+
+	/// Copy constructor (disabled)
+	CRingTower(const CRingTower&) = delete;
+
+	/**Saves item to XMLNode
+	*\param node
+	*\returns itemNode
+	*/
+	virtual std::shared_ptr<xmlnode::CXmlNode>
+		XmlSave(const std::shared_ptr<xmlnode::CXmlNode>& node) override;
+
+private:
+
 };
 
--- Towers/TowerEight.cpp
+++ Towers/TowerEight.cpp
@@ -1,2 +1,17 @@
 #include "pch.h"
 #include "TowerEight.h"
+#include <string>
+
+const std::wstring TowerEightName = L"images/tower8.png";
+
+CTowerEight::CTowerEight(CGame* game) :
+	CTower(game, TowerEightName)
+{
+}
+
+/**
+std::shared_ptr<xmlnode::CXmlNode>
+CTowerEight::XmlSave(const std::shared_ptr<xmlnode::CXmlNode>& node)
+{
+}
+*/
