1c807718df1cbc7c03629582ce8f98a6b781ee1f
Hithesh Yedlapati <yedlapat@msu.edu>
2020-10-15 11:52:54 -0400

    add comments for CTower and derived classes

--- Towers/Tower.cpp
+++ Towers/Tower.cpp
@@ -1,9 +1,19 @@
+/**
+ * \file Tower.cpp
+ *
+ * \author Hithesh Yedlapati
+ */
+
 #include "pch.h"
 #include "Tower.h"
 #include <string>
 
 class CGame;
 
+/**
+* Tower Constructor
+* \param game the whole game
+*/
 CTower::CTower(CGame* game) : CItem(game)
 {
 
